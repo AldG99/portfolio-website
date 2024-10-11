7f19c121639a55aaa0e4834d78bea9ce3332f24f
AldG99 <a.garciaglz17@gmail.com>
2024-10-10 22:55:31 -0600

    Hacer un desplazamiento suave entre secciones

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import Navbar from './components/navbar/navbar';
 
 const App = () => {
   return <div>
-    <section>
+    <section id='Inicio'>
       <Navbar/>
     </section>
-    <section><a>Hero</a></section>
-    <section>Parallax</section>
-    <section>Services</section>
-    <section>Pa</section>
-    <section>Hero</section>
-    <section>Hero</section>
-    <section>Hero</section>
-    <section>Hero</section>
-    <section>Hero</section>
+    <section id='Servicios'>Portafolio1</section>
+    <section id='Portafolio'>Portafolio2</section>
+    <section id='Contacto'>Portafolio3</section>
   </div>
-}
+};
 
 export default App;
--- src/components/sidebar/links/links.js
+++ src/components/sidebar/links/links.js
@@ -1,7 +1,20 @@
 const Links = () => {
+
+  const items = [
+    "Inicio",
+    "Servicios",
+    "Portafolio",
+    "Contacto",
+    "Acerca de"
+  ];
+
   return (
-    <div className="links">Links</div>
-  )
-}
+    <div className="links">{items.map(item=>(
+      <a href={`#${item}`} key={item}>
+        {item}
+      </a>
+    ))}</div>
+  );
+};
 
 export default Links;
