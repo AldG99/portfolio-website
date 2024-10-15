72e62c8b5f2608957813802c9241bcd8ef781ecb
AldG99 <a.garciaglz17@gmail.com>
2024-10-15 15:52:26 -0600

    Animaciones de enlaces del menú de la barra lateral

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './app.scss';
 import Navbar from './components/navbar/navbar';
 
 const App = () => {
-  return <div>
-    <section id='Inicio'>
-      <Navbar/>
-    </section>
-    <section id='Servicios'>Portafolio1</section>
-    <section id='Portafolio'>Portafolio2</section>
-    <section id='Contacto'>Portafolio3</section>
-  </div>
+  return (
+    <div>
+      <section id="Inicio">
+        <Navbar />
+      </section>
+      <section id="Servicios">Portafolio1</section>
+      <section id="Portafolio">Portafolio2</section>
+      <section id="Contacto">Portafolio3</section>
+    </div>
+  );
 };
 
 export default App;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,31 +1,39 @@
-import "../../styles/navbar/navbar.scss";
-import { motion } from "framer-motion";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import Sidebar from "../sidebar/sidebar";
+import '../../styles/navbar/navbar.scss';
+import { motion } from 'framer-motion';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import Sidebar from '../sidebar/sidebar';
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <Sidebar/>
+      <Sidebar />
       <div className="wrapper">
-        <motion.span 
-          initial={{ opacity: 0, scale: 0.5 }} 
-          animate={{ opacity: 1, scale: 1 }} 
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
           AG
         </motion.span>
         <div className="social">
-          <a href="https://github.com/Aldg99" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/Aldg99"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaGithub size={24} />
           </a>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaLinkedin size={24} />
           </a>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
