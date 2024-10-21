b807170d26c3fe19a6268e67f1ceb6ae4a7fcafc
AldG99 <a.garciaglz17@gmail.com>
2024-10-21 10:38:33 -0600

    Animar la vista Framer Motion

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -1,30 +1,58 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useRef } from 'react';
+import { motion, useInView } from 'framer-motion';
 import './services.scss';
 
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
 const Services = () => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: '-100px' });
+
   return (
-    <motion.div className="services">
-      <motion.div className="textContainer">
+    <motion.div
+      className="services"
+      variants={variants}
+      initial="initial"
+      ref={ref}
+      animate={isInView && 'animate'}
+    >
+      <motion.div className="textContainer" variants={variants}>
         <p>
           Me concentro en ayudar a que tu marca crezca <br /> y avance.
         </p>
         <hr />
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div className="titleContainer" variants={variants}>
         <div className="title">
           <h1>
-            <b>Ideas</b> únicas
+            <motion.b whileHover={{ color: 'orange' }}>Ideas</motion.b> únicas
           </h1>
         </div>
         <div className="title">
           <h1>
-            <b>Para sus ideas</b> de negocio.
+            <motion.b whileHover={{ color: 'orange' }}>Para sus ideas</motion.b>{' '}
+            de negocio.
           </h1>
           <button>¿QUÉ HAGO?</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer">
+      <motion.div className="listContainer" variants={variants}>
         <motion.div
           className="box"
           whileHover={{ background: 'lightgray', color: 'black' }}
